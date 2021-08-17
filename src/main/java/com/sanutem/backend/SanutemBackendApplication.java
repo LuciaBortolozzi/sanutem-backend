ea1e4999b6cfa863fa8977cec66f12cd3b5d775c
Lucia Bortolozzi <luciabortolozzi@gmail.com>
2021-08-16 22:39:56 -0300

    Initial commit

---
+++ src/main/java/com/sanutem/backend/SanutemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanutem.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanutemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanutemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanutem/backend/SanutemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanutem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanutemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
