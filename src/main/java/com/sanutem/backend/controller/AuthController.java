08d6fa93a849f84f498cc278da1b817acdef0704
Lucia Bortolozzi <luciabortolozzi@gmail.com>
2021-10-19 01:55:59 -0300

    Medical Tests + Appointments

--- src/main/java/com/sanutem/backend/controller/AuthController.java
+++ src/main/java/com/sanutem/backend/controller/AuthController.java
@@ -28,6 +28,7 @@ public class AuthController {
     private final RefreshTokenService refreshTokenService;
     private final UsersRepository usersRepository;
     private final PetsRepository petsRepository;
+    private final AppointmentsRepository appointmentsRepository;
     private final ProvincesRepository provincesRepository;
     private final SpecializationsRepository specializationsRepository;
     private final HealthInsurancesRepository healthInsurancesRepository;
@@ -93,6 +94,11 @@ public class AuthController {
     @GetMapping("/user-profile/{username}/pets")
     public List<Pets> getPets(@PathVariable String username) {
         return petsRepository.getPetsByUsername(username);
+    }
+
+    @GetMapping("/user-profile/{username}/search/{professional}/schedule")
+    public List<Appointments> getAppointments(@PathVariable String username) {
+        return appointmentsRepository.getAppointmentsByUsername(username);
     }
 
     @PostMapping("/update/")
--- src/main/java/com/sanutem/backend/repository/AppointmentsRepository.java
+++ src/main/java/com/sanutem/backend/repository/AppointmentsRepository.java
@@ -2,6 +2,15 @@ package com.sanutem.backend.repository;
 
 import com.sanutem.backend.model.Appointments;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AppointmentsRepository extends JpaRepository<Appointments, Integer> {
+
+    @Query("SELECT a.idAppointments FROM Appointments a WHERE a.userNameProfessional = ?1")
+    List<Integer> findIdAppointmentByUsername(String userNameProfessional);
+
+    @Query("SELECT a FROM Appointments a WHERE a.userNameProfessional = ?1")
+    List<Appointments> getAppointmentsByUsername(String userNameProfessional);
 }
